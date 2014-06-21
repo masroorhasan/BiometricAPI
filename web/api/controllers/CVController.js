23870615827f6beba477b5a873b041fd7f041330
masroorhasan <masroor.hasan.n@gmail.com>
2014-06-21 13:38:32 -0400

    working facial detection

--- web/api/controllers/CVController.js
+++ web/api/controllers/CVController.js
@@ -18,20 +18,13 @@ module.exports = {
       var path = require('path');
       var cv = CVService.cv;
 
-      // var img = path.dirname(require.main.filename);
-      // var img = path.resolve(__dirname, '../../assets/data/haarcascade_frontalface_alt.xml')
-      // console.log(img);
-      var img = path.resolve(__dirname, '../../assets/images/mona.png');
-      console.log(img);
-
-      cv.readImage(img, function(err, im) {
+      cv.readImage(path.resolve(__dirname, '../../assets/images/mona.png'), function(err, im) {
           im.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
               for(var i = 0; i < faces.length; i++) {
                   console.log(faces[i]);
                   var x = faces[i];
                   im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
               }
-              // console.log(path.resolve(__dirname, '../../assets/images/out.png'));
               im.save(path.resolve(__dirname, '../../assets/images/out.png')); 
           });
       });
