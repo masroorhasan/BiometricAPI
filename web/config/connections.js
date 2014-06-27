30f94aedf7d06531aa80542750db9ef5782c41ea
masroorhasan <masroor.hasan.n@gmail.com>
2014-06-27 12:55:44 -0400

    added sails-mongo connection

--- web/config/connections.js
+++ web/config/connections.js
@@ -59,7 +59,7 @@ module.exports.connections = {
     port: 27017,
     // user: 'username',
     // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    database: 'test'
   },
 
   // PostgreSQL is another officially supported relational database.
